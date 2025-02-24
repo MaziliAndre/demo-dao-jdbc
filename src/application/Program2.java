a88c3204cbebd1acd061df5f1ca77d9a09eb2188
André Mazili <maziliandre@gmail.com>
2025-02-24 18:11:01 -0300

    Insert implementation for department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -20,6 +20,10 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST 3: department insert");
+		Department newDepartment = new Department(null, "DVDs");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New ID: " + newDepartment.getId());
 
 	}
 
